5b799f6558c3c57b5197c8dbc6e9893ff4741e71
Iyneshwar <153883175+Iyneshwar@users.noreply.github.com>
2023-12-18 23:20:21 +0530

    Add files via upload

---
+++ Question_1/Main.java
@@ -0,0 +1,17 @@
+
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        Stream<String> names = Stream.of("abc", "d", "ef");
+
+        List<String> uppercaseList = names
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+
+        System.out.println("Uppercase List: " + uppercaseList);
+    }
+}
